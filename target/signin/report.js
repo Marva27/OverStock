5254529a6930af9232269c16a41d71ec9bf25053
SrinivasanRamu <SrinivasanRamu@192.168.0.101>
2016-07-27 16:25:08 -0400

    Trivial change

--- target/signin/report.js
+++ target/signin/report.js
@@ -1,4 +1,122 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("com/srinimarva/overstock/features/customer/SignIn.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("com/srinimarva/overstock/features/customer/ForgotPassword.feature");
+formatter.feature({
+  "comments": [
+    {
+      "line": 1,
+      "value": "#Author: Srinivasan Ramu"
+    },
+    {
+      "line": 2,
+      "value": "#Email: srinimarva@gmail.com"
+    }
+  ],
+  "line": 3,
+  "name": "To test the Forgot Password functionality",
+  "description": "",
+  "id": "to-test-the-forgot-password-functionality",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 5,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 6,
+  "name": "I open \"Chrome\" browser to execute \"ForgotPassword\" test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "I connect to \"http://www.overstock.com\" website",
+  "keyword": "And "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Chrome",
+      "offset": 8
+    },
+    {
+      "val": "ForgotPassword",
+      "offset": 36
+    }
+  ],
+  "location": "CommonStepDefs.i_Open_Browser(String,String)"
+});
+formatter.result({
+  "duration": 980518629,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "http://www.overstock.com",
+      "offset": 14
+    }
+  ],
+  "location": "CommonStepDefs.i_Connect_URL(String)"
+});
+formatter.result({
+  "duration": 675839,
+  "error_message": "java.lang.NullPointerException\n\tat com.srinimarva.overstock.stepdefs.common.CommonStepDefs.i_Connect_URL(CommonStepDefs.java:32)\n\tat ✽.And I connect to \"http://www.overstock.com\" website(com/srinimarva/overstock/features/customer/ForgotPassword.feature:7)\n",
+  "status": "failed"
+});
+formatter.scenario({
+  "line": 9,
+  "name": "Retrieve forgotten password with valid email address",
+  "description": "",
+  "id": "to-test-the-forgot-password-functionality;retrieve-forgotten-password-with-valid-email-address",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 10,
+  "name": "I navigate to Sign In page to execute \"ForgotPassword\" test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 11,
+  "name": "I click the Forgot your password? link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 12,
+  "name": "I should see Password Assistance page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "I enter a valid",
+  "keyword": "When "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "ForgotPassword",
+      "offset": 39
+    }
+  ],
+  "location": "SignIn.navigateToSignInPage(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.uri("com/srinimarva/overstock/features/customer/SignIn.feature");
 formatter.feature({
   "comments": [
     {
@@ -47,7 +165,7 @@ formatter.match({
   "location": "CommonStepDefs.i_Open_Browser(String,String)"
 });
 formatter.result({
-  "duration": 5847073837,
+  "duration": 7590023244,
   "status": "passed"
 });
 formatter.match({
@@ -60,7 +178,7 @@ formatter.match({
   "location": "CommonStepDefs.i_Connect_URL(String)"
 });
 formatter.result({
-  "duration": 4699298398,
+  "duration": 8708878728,
   "status": "passed"
 });
 formatter.scenario({
@@ -135,21 +253,21 @@ formatter.match({
   "location": "SignIn.navigateToSignInPage(String)"
 });
 formatter.result({
-  "duration": 2514102203,
+  "duration": 1769817346,
   "status": "passed"
 });
 formatter.match({
   "location": "SignIn.attemptLogin(DataTable)"
 });
 formatter.result({
-  "duration": 2579189396,
+  "duration": 2161701765,
   "status": "passed"
 });
 formatter.match({
   "location": "SignIn.greetCustomer(DataTable)"
 });
 formatter.result({
-  "duration": 78657503,
+  "duration": 78639876,
   "status": "passed"
 });
 formatter.background({
@@ -183,7 +301,7 @@ formatter.match({
   "location": "CommonStepDefs.i_Open_Browser(String,String)"
 });
 formatter.result({
-  "duration": 163866,
+  "duration": 170395,
   "status": "passed"
 });
 formatter.match({
@@ -196,7 +314,7 @@ formatter.match({
   "location": "CommonStepDefs.i_Connect_URL(String)"
 });
 formatter.result({
-  "duration": 4052601493,
+  "duration": 3997338167,
   "status": "passed"
 });
 formatter.scenario({
@@ -255,14 +373,14 @@ formatter.match({
   "location": "SignIn.navigateToSignInPage(String)"
 });
 formatter.result({
-  "duration": 2265876582,
+  "duration": 1808373944,
   "status": "passed"
 });
 formatter.match({
   "location": "SignIn.attemptLogin(DataTable)"
 });
 formatter.result({
-  "duration": 312979096,
+  "duration": 451174368,
   "status": "passed"
 });
 formatter.match({
