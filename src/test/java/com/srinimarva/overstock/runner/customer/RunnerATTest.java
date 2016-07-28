bcada5b6166b1f00a029291b32b6201925f7736f
SrinivasanRamu <SrinivasanRamu@192.168.0.101>
2016-07-28 12:10:44 -0400

    Included pretty format

--- src/test/java/com/srinimarva/overstock/runner/customer/RunnerATTest.java
+++ src/test/java/com/srinimarva/overstock/runner/customer/RunnerATTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources",
 glue={"com.srinimarva.overstock.stepdefs.common","com.srinimarva.overstock.stepdefs.customer"},
-format={"json:target/cucumber.json"},
+format={"pretty","json:target/cucumber.json"},
 tags={"@SignIn,@ForgotPassword"})
 public class RunnerATTest {
 
