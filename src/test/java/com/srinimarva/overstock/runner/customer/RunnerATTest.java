500fe816989eb40f8ab5bf5b09e9f121b4cc67b6
SrinivasanRamu <SrinivasanRamu@192.168.0.103>
2016-08-01 16:50:40 -0400

    Updated properties

--- src/test/java/com/srinimarva/overstock/runner/customer/RunnerATTest.java
+++ src/test/java/com/srinimarva/overstock/runner/customer/RunnerATTest.java
@@ -8,7 +8,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources",
 glue={"com.srinimarva.overstock.stepdefs.common","com.srinimarva.overstock.stepdefs.customer"},
-format={"pretty","json:target/cucumber.json"},
+format={"pretty","json:target/cucumber.json","html:target/index"},
 tags={"@SignIn,@ForgotPassword,@ChangePassword"})
 public class RunnerATTest {
 
